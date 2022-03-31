96787cf2c79c4f14b9a0730baa3bbf3d3862d32d
Antonio Bove <antoniofbove@gmail.com>
2022-03-31 15:22:30 +0200

    milestone 1

--- db.php
+++ db.php
@@ -1,6 +1,6 @@
 <?php
 
-$library = $dics = [
+$albums = [
   [
     'poster' => 'https://www.onstageweb.com/wp-content/uploads/2018/09/bon-jovi-new-jersey.jpg',
     'title' => 'New Jersey',
--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
 
+include __DIR__ . '/db.php';
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -8,8 +10,39 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>php-ajax-dischi</title>
+
+  <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
+
+  <div class="app-container">
+    <header></header>
+    <main>
+
+      <div class="container">
+        <div class="flex-row">
+          <?php
+          foreach($albums as $album):
+            extract($album);
+            ?>
+            <div class="card">
+              <div class="img-container">
+                <img src="<?= $poster ?>" alt="">
+              </div>
+              <h4 class="title"><?= strtoupper($title) ?></h4>
+              <div class="info-box">
+                <div class="author"><?= $author ?></div>
+                <div class="year"><?= $year ?></div>
+              </div>
+            </div>
+            <?php
+          endforeach
+          ?>
+        </div>
+      </div>
+
+    </main>
+  </div>
   
 </body>
 </html>
