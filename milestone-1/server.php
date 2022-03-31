78acec6b65437437acf060bba74f641ce176fc0f
Antonio Bove <antoniofbove@gmail.com>
2022-03-31 21:33:31 +0200

    server.php -> filerAlbums funtion fixed

--- milestone-1/server.php
+++ milestone-1/server.php
@@ -1,37 +1,25 @@
 <?php
-include_once __DIR__ . '/db.php';
 
 function filterAlbums(array $albums, string $genre): array {
   $arr = [];
-
   foreach($albums as $album) {
     if ($album['genre'] == $genre) {
       $arr[] = $album;
     }
   }
-  var_dump($arr);
 
   return $arr;
 }
 
+include_once __DIR__ . '/db.php';
 
-// $filter = array_keys($albums);
 $filter = isset($_GET['genre']) ? $_GET['genre'] : '';
 
 header('Content-Type: application/json; charset=utf-8');
 header('Access-Control-Allow-Origin: *');
 
 if ($filter != '' && $filter != "All") {
-  
-  $arr = [];
-  foreach($albums as $album) {
-    if ($album['genre'] == $filter) {
-      $arr[] = $album;
-    }
-  }
-
-  echo json_encode($arr);
-} else {
-
-  echo json_encode($albums);
+  $albums = filterAlbums($albums, $filter);
 }
+
+echo json_encode($albums);
