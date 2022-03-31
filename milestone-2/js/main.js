ffa08b16fb3a74d06106fe12024938de028ce663
Antonio Bove <antoniofbove@gmail.com>
2022-03-31 16:48:19 +0200

    API call ok

--- milestone-2/js/main.js
+++ milestone-2/js/main.js
@@ -2,11 +2,16 @@ const app = new Vue ({
   el: '#app',
   data: {
     albums: [],
+    genre: '',
   },
   methods: {
     fetchData() {
       axios
-        .get('http://localhost:8888/php-ajax-dischi/milestone-1/server.php')
+        .get('http://localhost:8888/php-ajax-dischi/milestone-1/server.php', {
+          params: {
+            genre: this.genre
+          }
+        })
         .then(res => {
           this.albums = res.data;
         })
